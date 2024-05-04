811be458e4deee68a492c0ffbae10d8c5cdfcbca
Noah Kay <noahskay@icloud.com>
2024-05-03 18:38:04 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'x-ops',
+  packages = find_packages(exclude=['examples']),
+  version = '0.0.1',
+  license='MIT',
+  description = 'X-Transformers - Pytorch',
+  author = 'Noah Kay',
+  author_email = 'noahkay13@gmail.com',
+  url = 'https://github.com/Frikallo/x-ops',
+  long_description_content_type = 'text/markdown',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers'
+  ],
+  install_requires=[
+    'torch>=1.6',
+    'einops>=0.7.0'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
